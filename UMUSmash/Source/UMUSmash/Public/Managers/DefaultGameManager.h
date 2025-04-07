c1bec277069d7d4cc0e25199c4e561e8c284f371
LeeDoHyun <ehugstjstod2003@gmail.com>
2025-04-07 13:15:59 +0900

    Fix Bug

--- UMUSmash/Source/UMUSmash/Public/Managers/DefaultGameManager.h
+++ UMUSmash/Source/UMUSmash/Public/Managers/DefaultGameManager.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "UMUGameMode.generated.h"
+#include "DefaultGameManager.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class UMUSMASH_API AUMUGameMode : public AGameModeBase
+class UMUSMASH_API ADefaultGameManager : public AGameModeBase
 {
 	GENERATED_BODY()
 	
