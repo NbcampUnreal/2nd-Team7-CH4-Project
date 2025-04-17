6da02e7aac563593176736bf562d376925669954
KimLink <Linkedcirrus93@gmail.com>
2025-04-17 11:31:02 +0900

    MergeTest

---
+++ UMUSmash/Source/UMUSmash/Private/UMU_Lobbycontroller.cpp
@@ -0,0 +1,13 @@
+//#include "UMU_LobbyController.h"
+//#include "Kismet/GameplayStatics.h"
+//
+//
+//UMUGameInstance* AUMU_LobbyController::GetMyGameInstance()
+//{
+//    return Cast<UMUGameInstance>(GetGameInstance());
+//}
+//
+//UMUFightGameMode* AUMU_LobbyController::GetMyGameMode()
+//{
+//    return Cast<UMUFightGameMode>(UGameplayStatics::GetGameMode(this));
+//}
---
+++ UMUSmash/Source/UMUSmash/Public/UMU_Lobbycontroller.h
@@ -0,0 +1,18 @@
+//#pragma once
+//
+//#include "Managers/UMUGameInstance.h"
+//#include "Managers/UMUFightGameMode.h"
+//#include "CoreMinimal.h"
+//#include "GameFramework/PlayerController.h"
+//#include "UMU_LobbyController.generated.h"
+//
+//
+//UCLASS()
+//class UMUSMASH_API AUMU_LobbyController : public APlayerController
+//{
+//	GENERATED_BODY()
+//	
+//public:
+//	class UMUGameInstance* GetMyGameInstance();
+//	class UMUFightGameMode* GetMyGameMode();
+//};
